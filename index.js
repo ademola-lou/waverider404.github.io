eeece3bffc013b0cf12612787f547998f6a494b9
Glitch (regal-free-weight) <none>
2020-08-03 18:42:22 +0000

    🍢🌌 Checkpoint ./index.js:1101202/1876 ./index.html:1101202/1000

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ function closeabout(){
     },800);
 }
 
-function closepage(){
-  
+function closepage(id){
+   $(id).addClass("animated slideOutLeft");
+    setTimeout(function(){
+        $(id).removeClass("animated slideOutLeft");
+        $(id).css("display","none");
+    },800);
 }
 function showwork(){
     $("#work_container").css("display","inherit");
@@ -50,6 +54,43 @@ function openTutorial(id){
     setTimeout(function(){
     $(id).removeClass("animated slideInUp");
     },800);
+}
+function embedvideo(videoId){
+   var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: videoId,
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
 }
 setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
